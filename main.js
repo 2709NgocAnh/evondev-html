c32423f2ac6b9aa990bb849619f84044ed3db01b
Anh Ngọc <1951120082@sv.ut.edu.vn>
2022-04-04 12:23:29 +0700

    input beauty

---
+++ main.js
@@ -0,0 +1,36 @@
+// alert('Phan Thi Ngoc Anh');
+// var a=1 ;
+// var b=2;
+// var isSuccess=a>b;
+// console.log(isSuccess);
+
+// // cau dieu kien
+// if(isSuccess){
+//     console.log)'Dieu kien dung');
+// }else{
+//     console.log('Dieu kien sai')
+// }
+// //toan tu logical
+// var a=1;
+// var a=2;
+// var a=3;
+// if (!(a>o)){
+//     console.log('Dieu kien dung');
+// }
+//function and Object
+// var myObject = {
+//     name : 'Na',
+//     age : 18,
+//     address:'Binh Phuoc'
+// }
+// console.log('thong tin',myObject)
+// // biến
+// var fullName='Na na annananananananana'
+// +'ânnanananananananananna';
+// console.log(fullName);
+var myName = '   Phan Thi Ngoc Anh   ';
+console.log(myName.split(' '));
+// confirm('xac nhan ban bao nhieu tuoi');
+// prompt('xac nhan ban bao nhieu tuoi');
+console.log(myName.replace(/h/g,'b'));
+console.log(myName.slice(5,9));
